a9a771ce0f1bdbeb3cb1be1183acc6af0e025853
Birbal S Naik <112779904+Birbalsarva@users.noreply.github.com>
2023-09-21 19:10:21 +0530

    Create deploy-ec2.sh

---
+++ deploy-ec2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Change to the project directory
+cd /path/to/your-mern-project
+
+# Pull the latest code from your Git repository
+git pull
+
+# Build and serve the application using Docker Compose
+docker-compose -f docker-compose.ec2.yml up -d
+
+# Wait for containers to fully start (you may need to customize this)
+sleep 10
+
+# Run any additional setup or migrations if needed
+# Example: docker-compose exec backend npm run migrate
+
+echo "Deployment on EC2 complete!"
