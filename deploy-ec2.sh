07fb5481ce70eb98ae03bc75465f29178f15e5fc
Birbal S Naik <112779904+Birbalsarva@users.noreply.github.com>
2023-09-22 18:06:56 +0530

    Update deploy-ec2.sh

--- deploy-ec2.sh
+++ deploy-ec2.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-
-cd /home/ubuntu/mern/MERN-Sample-Social-Media-App
+cd /home/ubuntu/Real-IT-Solutions-assignmet
 
 # Build and serve the application using Docker Compose
 docker-compose -f docker-compose.ec2.yml up -d
 
-# Wait for containers to fully start 
+# Wait for containers to fully start (you may need to customize this)
 sleep 10
 
 echo "Deployment on EC2 complete!"
